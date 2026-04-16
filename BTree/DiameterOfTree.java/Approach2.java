4f636fe91dca02dcda4fc8af94023f51ba83c117
UtkarshGupta05 <utkarshguptag60@gmail.com>
2026-04-16 22:03:22 +0530

    BTree

---
+++ BTree/DiameterOfTree.java/Approach2.java
@@ -0,0 +1,49 @@
+public class Approach2 {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node (int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    static class BinaryTree{
+        static int idx=-1;
+        public static Node BuildTree(int[] nodes){
+            idx++;
+            if(nodes[idx]==-1){
+                return null;
+            } 
+            Node newNode=new Node(nodes[idx]);
+            newNode.left=BuildTree(nodes);
+            newNode.right=BuildTree(nodes);
+            return newNode;
+        }
+    }
+    public static int height(Node root){
+        if(root==null){
+            return 0;
+        }
+        int lh=height(root.left);
+        int rh=height(root.right);
+        return Math.max(lh,rh)+1;
+        
+    }
+    public static int DiameterOfTree(Node root ){
+        if(root==null) return 0;
+        int lDia=DiameterOfTree(root.left);
+        int lh=height(root.left);
+        int rDia=DiameterOfTree(root.right);
+        int rh=height(root.right);
+
+        int selfDia=lh+rh;
+        return Math.max(selfDia,Math.max(lDia,rDia));
+    }
+    public static void main(String[] args) {
+        int[] nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        Node root=BinaryTree.BuildTree(nodes);
+        System.out.print("Diameter of Tree is : " +Approach2.DiameterOfTree(root));
+    }
+}
